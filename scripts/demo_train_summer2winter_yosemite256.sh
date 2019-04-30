31d60d7bb822cb4e1d48f7676069c99d0d29680b
cwx <chenWWWeixiang>
2019-04-30 09:57:02 +0800

    11

--- scripts/demo_train_summer2winter_yosemite256.sh
+++ scripts/demo_train_summer2winter_yosemite256.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm datasets/summer2winter_yosemite256 -p
 mkdir datasets/summer2winter_yosemite256 -p
-axel -n 1 https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/summer2winter_yosemite.zip --output=datasets/summer2winter_yosemite256/summer2winter_yosemite.zip
-unzip datasets/summer2winter_yosemite256/summer2winter_yosemite.zip -d datasets/summer2winter_yosemite256
-python train.py --config configs/summer2winter_yosemite256_folder.yaml
+wget https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/summer2winter_yosemite.zip
+unzip summer2winter_yosemite256/summer2winter_yosemite.zip -d datasets/summer2winter_yosemite256
+python CUDA_VISIBLE_DEVICES=0 train.py --config configs/summer2winter_yosemite256_folder.yaml
